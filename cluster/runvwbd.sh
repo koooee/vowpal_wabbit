0344c21ac1b1d37cb655dd3c5f188c29255790d3
koooee <nick@winthropgaming.com>
2012-06-06 12:33:00 +0000

    minor changes for clustering

--- cluster/runvwbd.sh
+++ cluster/runvwbd.sh
@@ -1,6 +1,8 @@
+# TODO: how do we wrap an interface around this?
+# FIXME: We are using a random number that isn't really a unique ID
 #vw -b 24 --total 4 --node 4 --unique_id $RANDOM --cache_file temp.cache --spanserver master0 -f vwmodel --passes 1 --adaptive --exact_adaptive_norm -d /dev/stdin --regularization=1 --loss_function=logistic
 
-./vw /dev/stdin -b 18 -q ui --rank 10 --l2 0.001 \
+./vw /dev/stdin -b 18 --total 4 --node 4 --unique_id $RANDOM -q ui --rank 10 --l2 0.001 \
   --learning_rate 0.025 --passes 20 --decay_learning_rate 0.97 --power_t 0 \
   -f movielens.reg --cache_file movielens.cache
 
--- cluster/vwwrapper.sh
+++ cluster/vwwrapper.sh
@@ -1,3 +1,3 @@
-# Wrapper for Vowpal Wabbit on BigDataR Linux
-# FIXME: We are using a random number that isn't really a unique ID
+# Wrapper to launch Vowpal Wabbit on BigDataR Linux
+
 hadoop jar $HADOOP_HOME/contrib/streaming/hadoop-*streaming*.jar -Dmapred.job.map.memory.mb=512 -input $1 -output $2 -file /usr/local/bin/vw -file /home/play/runvwbd.sh -mapper 'runvwbd.sh' -reducer NONE
