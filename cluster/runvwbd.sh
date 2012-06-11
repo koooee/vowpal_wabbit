5391666256c3494b72170ee4e9368317dcc3608e
koooee <nick@winthropgaming.com>
2012-06-11 05:18:56 +0000

    added debugging features

--- cluster/runvwbd.sh
+++ cluster/runvwbd.sh
@@ -2,7 +2,31 @@
 # FIXME: We are using a random number that isn't really a unique ID
 #vw -b 24 --total 4 --node 4 --unique_id $RANDOM --cache_file temp.cache --spanserver master0 -f vwmodel --passes 1 --adaptive --exact_adaptive_norm -d /dev/stdin --regularization=1 --loss_function=logistic
 
-./vw /dev/stdin -b 18 --total 4 --node $(hostname | grep -o [0-9]*) --unique_id $RANDOM --spanserver master0 -q ui --rank 10 --l2 0.001 \
-  --learning_rate 0.025 --passes 20 --decay_learning_rate 0.97 --power_t 0 \
-  -f movielens.reg --cache_file movielens.cache
+mapper=`printenv mapred_task_id | cut -d "_" -f 5`
+echo "mapper: $mapper" > /dev/stderr
+rm -f temp.cache
+echo 'Starting training' > /dev/stderr
+echo $1 > /dev/stderr
+mapred_job_id=`echo $mapred_job_id | tr -d 'job_'`
+echo "mapred_job_id: $mapred_job_id" > /dev/stderr
+echo "mapred_map_tasks: $mapred_map_tasks" > /dev/stderr
+echo "mapreduce_job_submithost: $mapreduce_job_submithost" > /dev/stderr
+echo "Data Sample:" > /dev/stderr
+(head -n 4 /dev/stdin) > /dev/stderr
+./vw \
+-d /dev/stdin \
+-b 18 \
+--total $mapred_map_tasks \
+--node $(hostname | grep -o [0-9]*) \
+--unique_id $mapred_job_id \
+--span_server master0 \
+-q ui \
+--rank 10 \
+--l2 0.001 \
+--learning_rate 0.025 \
+--passes 20 \
+--decay_learning_rate 0.97 \
+--power_t 0 \
+-f movielens.reg \
+--cache_file movielens.cache
 
--- cluster/vwwrapper.sh
+++ cluster/vwwrapper.sh
@@ -1,3 +1,11 @@
 # Wrapper to launch Vowpal Wabbit on BigDataR Linux
-
-hadoop jar $HADOOP_HOME/contrib/streaming/hadoop-*streaming*.jar -Dmapred.job.map.memory.mb=512 -input $1 -output $2 -file /usr/local/bin/vw -file /home/play/runvwbd.sh -mapper 'runvwbd.sh' -reducer NONE
+spanning_tree
+hadoop jar $HADOOP_HOME/contrib/streaming/hadoop-*streaming*.jar \
+-Dmapred.job.map.memory.mb=2500 \
+-Dmapred.task.timeout=600000000 \
+-input $1 \
+-output $2 \
+-file /usr/local/bin/vw \
+-file ./runvwbd.sh \
+-mapper 'runvwbd.sh' \
+-reducer NONE
